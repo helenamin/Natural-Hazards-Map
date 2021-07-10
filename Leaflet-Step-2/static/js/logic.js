326fe6d27b57ec0391f16722378c4abcc5865ea5
helenamin <helen.amin30@yahoo.com>
2021-07-10 12:41:33 +0800

    step-2 finalised

--- Leaflet-Step-2/static/js/logic.js
+++ Leaflet-Step-2/static/js/logic.js
@@ -35,7 +35,7 @@ var baseMaps = {
 
 // Create map object and set default layers
 var myMap = L.map("map", {
-  center: [15, 20],
+  center: [40, -74],
   zoom: 3,
   layers: [satellite]
 });
@@ -53,28 +53,42 @@ function getColor(d) {
 // Store our API endpoint as queryUrl
 var query_earthquake = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
-var query_plates = "https://github.com/fraxen/tectonicplates/blob/master/GeoJSON/PB2002_boundaries.json"
+var query_plates = "static/data/PB2002_boundaries.json"
+
 
 // Perform a GET request to the query URL
-d3.json(query_earthquake).then(function(data) {
-    var features = data.features;
+d3.json(query_earthquake).then(function(eq_data) {
+  d3.json(query_plates).then(function(p_data) {
+
+    var features = eq_data.features;
+    var earthquake_arr =[]
     
-    // Using the features array sent back in the API data, create a GeoJSON layer and add it to the map
+
+    // Using the features array create earth quake layer and add it to the map
     features.forEach(record =>{
       console.log("meg : "+record.properties.mag);
       if(record.geometry){
-          // console.log([record.geometry.coordinates[1], record.geometry.coordinates[0]])
-          var circle_layer = L.circle([record.geometry.coordinates[1], record.geometry.coordinates[0]], {
-            color: getColor(record.properties.mag),
-            fillColor: getColor(record.properties.mag),
-            fillOpacity: 1,
-            radius: record.properties.mag *20000
-          }).addTo(myMap);
-          // Binding a pop-up to each layer
-          circle_layer.bindPopup(`${record.properties.title} <br>Time: ${new Date(record.properties.time).toLocaleString()}`)
-          .addTo(myMap);
-        }
-      });
+        
+        var circle_layer = L.circle([record.geometry.coordinates[1], record.geometry.coordinates[0]], {
+          color: getColor(record.properties.mag),
+          fillColor: getColor(record.properties.mag),
+          fillOpacity: 1,
+          radius: (record.properties.mag *95)**2
+        });
+        
+        // Binding a pop-up to the layer
+        circle_layer.bindPopup(`${record.properties.title} <br>Time: ${new Date(record.properties.time).toLocaleString()}`);
+        
+        earthquake_arr.push(circle_layer);
+      }
+    })
+
+    var earthquake_layer = L.layerGroup(earthquake_arr).addTo(myMap);
+
+    // Add Plate Layer to the map
+    var plateLayer = L.geoJson(p_data).addTo(myMap);
+
+
 
 
     // Set up the legend
@@ -97,30 +111,21 @@ d3.json(query_earthquake).then(function(data) {
     return div;
   };
 
-  // d3.json(query_plates).then(function(data) {
-  //   L.geoJson(data, {
-  //     // Style each feature (in this case a neighborhood)
-  //     style: function(feature) {
-  //       return {
+  // Adding legend to the map
+  legend.addTo(myMap);
 
-  //       }
-  //     }
-  //   })
-  //   });
-  // })
+  // Overlays that may be toggled on or off
+  var overlayMaps = {
+    "Earthquakes": earthquake_layer,
+    "Fault Lines": plateLayer 
+  };
 
-
-    // Adding legend to the map
-    legend.addTo(myMap);
-
-    })
-
-// Overlays that may be toggled on or off
-var overlayMaps = {
-};
-
-
-    // Pass our map layers into our layer control
 // Add the layer control to the map
-L.control.layers(baseMaps, overlayMaps).addTo(myMap);
+L.control.layers(baseMaps,overlayMaps).addTo(myMap);
+
+
+  })
+})
+
+
 
